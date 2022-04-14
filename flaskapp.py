4ac72c37d65e8e47b7c66660666214d810f5ea5b
Aditya Mitra <adityaarghya0@gmail.com>
2022-04-14 23:05:08 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -29,7 +29,7 @@ import uuid
 import re
 import pytz
 
-url="payments.centralindia.cloudapp.azure.com"
+url="pp2pp.centralindia.cloudapp.azure.com"
 filepth='/home/vm_user/medrecords/'
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
